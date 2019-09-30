ac2a00f4fc80e74d06efe77e53e1dcf34af7c377
Kevin <khk4084417@gmail.com>
2019-09-30 14:58:14 -0700

    create class is created

---
+++ classes.py
@@ -0,0 +1,13 @@
+
+class Kevin:
+    def kevin(self):
+        print('help')
+
+    def yo(self):
+        print('oh yeah')
+
+def main():
+    for n in range(100):
+        print(n)
+
+    print('hello')
